f8477381b6ead828eaaecf6c24f0e008c628faac
Vladimir Mitic <miticv@gmail.com>
2014-10-14 10:30:14 -0400

    Directive examples for typescript

--- OwinAngularBoilerplate/app/main/directives/directives.js
+++ OwinAngularBoilerplate/app/main/directives/directives.js
@@ -2,20 +2,38 @@
 'use strict';
 var app;
 (function (app) {
+    //#region Way 1
     (function (directives) {
-        var MyDirective = (function () {
-            function MyDirective() {
-                this.template = '<div></div>';
+        var MyDirective1 = (function () {
+            function MyDirective1() {
                 this.restrict = 'E';
+                this.template = '<div></div>';
             }
-            MyDirective.prototype.link = function ($scope, element, attrs) {
-                element.text('this is the MyDirective directive');
+            MyDirective1.prototype.link = function ($scope, element, attrs) {
+                element.text('this is the MyDirective1 directive');
             };
-            return MyDirective;
+            return MyDirective1;
         })();
-        directives.MyDirective = MyDirective;
+        directives.MyDirective1 = MyDirective1;
     })(app.directives || (app.directives = {}));
     var directives = app.directives;
 })(app || (app = {}));
-//app.application.directive(app.directives.MyDirective);//'myDirective',
+
+//app.application.directive('myDirective1', function (): ng.IDirective { return new app.directives.MyDirective1() });
+app.application.directive('myDirective1', function () {
+    return new app.directives.MyDirective1();
+});
+
+//#endregion
+//#region Way 2
+app.application.directive('myDirective2', function () {
+    return {
+        restrict: 'E',
+        template: '<div></div>',
+        link: function ($scope, element, attrs) {
+            element.text('this is the MyDirective2 directive');
+        }
+    };
+});
+//#endregion
 //# sourceMappingURL=directives.js.map
--- OwinAngularBoilerplate/app/main/directives/directives.ts
+++ OwinAngularBoilerplate/app/main/directives/directives.ts
@@ -2,17 +2,31 @@
      
 'use strict';
 
+//#region Way 1
 module app.directives {
 
-    export class MyDirective implements IDirective {
-        template = '<div></div>';
-        restrict = 'E';
-        link($scope, element: JQuery, attrs: ng.IAttributes) {
-            element.text('this is the MyDirective directive');
-        }
-
+    export class MyDirective1 implements ng.IDirective {
+         
+        public restrict = 'E';
+        public template = '<div></div>';
+        public link($scope: ng.IScope, element: JQuery, attrs: ng.IAttributes) {
+            element.text('this is the MyDirective1 directive')
+        }        
     }
-
 }
+//app.application.directive('myDirective1', function (): ng.IDirective { return new app.directives.MyDirective1() });
+app.application.directive('myDirective1', () => new app.directives.MyDirective1());
+//#endregion
 
-//app.application.directive(app.directives.MyDirective);//'myDirective', 
+
+//#region Way 2
+app.application.directive('myDirective2', function (): ng.IDirective {
+    return {
+        restrict: 'E',
+        template: '<div></div>',
+        link: function ($scope: ng.IScope, element: JQuery, attrs: ng.IAttributes) {
+            element.text('this is the MyDirective2 directive')
+        }
+    };
+});
+//#endregion
